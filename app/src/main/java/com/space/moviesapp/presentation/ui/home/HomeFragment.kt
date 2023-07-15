623b6d27e2f07e7bfca2617f015936e212c5c96f
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2023-07-15 15:14:04 +0400

    add loader layout

--- app/src/main/java/com/space/moviesapp/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/space/moviesapp/presentation/ui/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.space.moviesapp.presentation.ui.home
 
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
+import com.space.moviesapp.R
 import com.space.moviesapp.databinding.FragmentHomeBinding
 import com.space.moviesapp.presentation.base.fragment.BaseFragment
 
@@ -10,5 +12,7 @@ class HomeFragment :
     override val viewModel: HomeViewModel by viewModels()
 
     override fun onBind() {
+        DialogFragment(R.layout.layout_loader)
+            .show(childFragmentManager, "loader")
     }
 }
