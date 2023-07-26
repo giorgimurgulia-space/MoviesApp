3554a2dc16c7752140d6f54d74ce5ac0e443f1f2
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2023-07-26 14:32:54 +0400

    add apiService fun

--- app/src/main/java/com/space/moviesapp/data/remote/api/ApiService.kt
+++ app/src/main/java/com/space/moviesapp/data/remote/api/ApiService.kt
@@ -3,8 +3,15 @@ package com.space.moviesapp.data.remote.api
 import com.space.moviesapp.data.remote.dto.MoviesDto
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("dbdf9ee9-8083-425a-99f0-4850c8f87cc6")
-    suspend fun getAllMovies(): Response<List<MoviesDto>>
+    @GET("movie/popular")
+    suspend fun getPopularMovies(): Response<MoviesDto>
+
+    @GET("search/top_rated")
+    suspend fun getTopMovies(): Response<MoviesDto>
+
+    @GET("search/movie")
+    suspend fun searchMovies(@Query("query") query: String): Response<MoviesDto>
 }
