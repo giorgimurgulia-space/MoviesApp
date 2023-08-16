4c91451dc5b19f6b32b9268066c3970a5deef700
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2023-08-16 18:34:08 +0400

    add bottom navigation

--- app/src/main/java/com/space/moviesapp/activity/MoviesActivity.kt
+++ app/src/main/java/com/space/moviesapp/activity/MoviesActivity.kt
@@ -2,6 +2,10 @@ package com.space.moviesapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.space.moviesapp.R
 import com.space.moviesapp.databinding.ActivityMoviesBinding
 
 class MoviesActivity : AppCompatActivity() {
@@ -12,5 +16,24 @@ class MoviesActivity : AppCompatActivity() {
 
         binding = ActivityMoviesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(0).isEnabled = false
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.detailsFragment -> {
+                    binding.bottomNavigationView.visibility = View.GONE
+                }
+                else -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
